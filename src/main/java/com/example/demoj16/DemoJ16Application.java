915946604b859ea7bb6069bb17f66e194f0e34e2
kjell <kjell1906@gmail.com>
2021-04-02 21:29:02 +0200

    Demonstrated switch between Tomcat and Jetty

--- src/main/java/com/example/demoj16/DemoJ16Application.java
+++ src/main/java/com/example/demoj16/DemoJ16Application.java
@@ -2,12 +2,37 @@ package com.example.demoj16;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoJ16Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJ16Application.class, args);
 	}
 
+	@GetMapping
+	Customer customer() {
+		return new Customer(1L, "James Bond");
+	}
+
+	class Customer{
+		private final Long id;
+		private final String name;
+
+		public Customer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
 }
