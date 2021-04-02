548f8c7e5a63b2dbc798509f77a55f8270a354ec
kjell <kjell1906@gmail.com>
2021-04-02 21:12:34 +0200

    Bootstrap project

---
+++ src/main/java/com/example/demoj16/DemoJ16Application.java
@@ -0,0 +1,13 @@
+package com.example.demoj16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJ16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJ16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoj16/DemoJ16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoj16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJ16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
