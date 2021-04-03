f93b7f7533ee294bb1f3b9d049442bd5ad0dfe84
kjell <kjell1906@gmail.com>
2021-04-03 13:47:09 +0200

    Added some nice startup beans in CustomerConfiguration

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,2 +1,31 @@
-package com.example.demo.customer;public class CustomerConfiguration {
+package com.example.demo.customer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Starts when the application starts. Nice to use when we
+ * need to run som config before startup.
+ */
+@Configuration
+public class CustomerConfiguration {
+    @Value("${app.useFakeCustomerRepo:false}")
+    private Boolean useFakeCustomerRepo;
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("CommandLineRunner hooray");
+        };
+    }
+
+    @Bean
+    CustomerRepo customerRepo() {
+        System.out.println("useFakeCustomerRepo = " + useFakeCustomerRepo);
+        return useFakeCustomerRepo ?
+                new CustomerFakeRepository() :
+                new CustomerRepository();
+    }
 }
