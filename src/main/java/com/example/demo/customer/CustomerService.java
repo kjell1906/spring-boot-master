84f85d9add829c243a037d82f5fdf1cc0406a37b
kjell <kjell1906@gmail.com>
2021-04-03 21:06:36 +0200

    Added Logging to project

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +10,9 @@ import java.util.List;
 
 @Component
 public class CustomerService {
+
+    private final static Logger LOGGER =
+            LoggerFactory.getLogger(CustomerService.class);
 
     private CustomerRepository customerRepository;
 
@@ -18,12 +23,19 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long customerId) {
+
         return customerRepository
                 .findById(customerId)
-                .orElseThrow(() -> new NotFoundException("Customer " + customerId +" not found"));
+                .orElseThrow(() -> {
+                    NotFoundException notFoundException =
+                            new NotFoundException("Customer " + customerId + " not found");
+                    LOGGER.error("errorGetCustomer", notFoundException);
+                    return notFoundException;
+                });
     }
 }
